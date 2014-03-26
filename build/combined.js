5bbeb21af0bedbaa3293e6a6086448c224318127
BillMills <mills.wj@gmail.com>
2014-03-26 11:16:53 -0700

    first use of localStorage to persist scale settings - debugging

--- build/combined.js
+++ build/combined.js
@@ -427,6 +427,8 @@ Math.log10 = function(x){
 
 //returns a if it isn't undefined or null, returns b otherwise
 function canHas(a, b){
+	console.log([a,b])
+
 	if(a === undefined || a === null) return b;
 
 	return a;
