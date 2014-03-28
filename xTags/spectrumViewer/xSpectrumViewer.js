c898cf824294430f28bd11b203dbc7b233a99346
BillMills <mills.wj@gmail.com>
2014-03-28 13:25:18 -0700

    hacking together an embedded spectrum viewer demo

---
+++ xTags/spectrumViewer/xSpectrumViewer.js
@@ -0,0 +1,39 @@
+//status bar
+(function(){  
+
+    xtag.register('widget-spectrumViewer', {
+        extends: 'div',
+        lifecycle: {
+            created: function() {
+                var title = document.createElement('h2')
+                ,   spectrum = document.createElement('canvas')
+
+                //headline
+                title.setAttribute('id', 'SVTitle');
+                this.appendChild(title);
+
+                //spectrum canvas
+                spectrum.setAttribute('id', 'SVplot');
+                this.appendChild(spectrum);                
+                
+            },
+            inserted: function() {},
+            removed: function() {},
+            attributeChanged: function() {}
+        }, 
+        events: { 
+
+        },
+        accessors: {
+
+        }, 
+        methods: {
+
+            'update': function(){
+               
+         
+            }
+        }
+    });
+
+})();
