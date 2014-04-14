9d91b6a3333a120d54e4dd18951e7c69e2671d01
BillMills <mills.wj@gmail.com>
2014-04-14 15:39:17 -0700

    first stab at TIGRESS & multiview detector

---
+++ xTags/detectors/TIGRESS/xTIGRESSDetector.js
@@ -0,0 +1,97 @@
+//status bar
+(function(){  
+
+    xtag.register('detector-TIGRESS', {
+        //prototype: Object.create(HTMLElement.prototype),
+        extends: 'detector-template',
+        lifecycle: {
+            created: function() {
+                //channels start at top left hand corner and walk across in rows
+                var channels = ['TPW011P00X', 'TPW012P00X', 'TPW013P00X', 'TPW014P00X', 'TPW015P00X',
+                                'TPW010P00X', 'TPW002P00X', 'TPW003P00X', 'TPW004P00X', 'TPW016P00X',
+                                'TPW009P00X', 'TPW001P00X', 'TPW005P00X', 'TPW017P00X',
+                                'TPW024P00X', 'TPW008P00X', 'TPW007P00X', 'TPW006P00X', 'TPW018P00X',
+                                'TPW023P00X', 'TPW022P00X', 'TPW021P00X', 'TPW020P00X', 'TPW019P00X'
+                                ]
+                    
+                    URLs = [this.thresholdServer,    //threshold server
+                            this.rateServer,             //rate server
+                            'http://'+window.location.host+'/?cmd=jcopy&odb0=Equipment/&encoding=json-p-nokeys&callback=fetchODBEquipment'];  //ODB Equipment tree
+
+                //deploy the standard stuff
+                initializeDetector.bind(this, 'TIGRESS', channels, 'TIGRESS', URLs, ['Main', 'TIG01', 'TIG02'])();
+/*
+                //////////////////////////////////////
+                //TIP Wall specific drawing parameters
+                //////////////////////////////////////
+                this.cellSide = this.height*0.8/5;              //length of cell side
+                this.x0 = this.width/2 - 2.5*this.cellSide;     //x coordinate of upper left corner of TIP image
+                this.y0 = 0;                                    //y ''
+
+                /////////////////////////////
+                //Initialize visualization
+                /////////////////////////////
+                //initialize all the cells:
+                this.instantiateCells();
+                //generate the color scale
+                this.generateColorScale();
+*/
+            },
+            inserted: function() {},
+            removed: function() {},
+            attributeChanged: function() {}
+        }, 
+        events: { 
+
+        },
+        accessors: {
+            'rateServer':{
+                attribute: {} //this just needs to be declared
+            },
+            'thresholdServer':{
+                attribute: {} //this just needs to be declared
+            }
+        }, 
+        methods: {
+            'instantiateCells': function(){
+                var i, iOffset, X, Y;
+
+                //each channel listed in this.channelNames gets an entry in this.cells as a Kinetic object:
+                for(i=0; i<this.channelNames.length; i++){
+                    iOffset = i;
+                    if(i>11) iOffset++; //skip the middle square in the grid
+
+                    //coords of top left corner of this cell
+                    X = this.x0 + this.cellSide*(iOffset%5);    
+                    Y = this.y0 + this.cellSide*Math.floor(iOffset/5);
+
+                    this.cells[this.channelNames[i]] = new Kinetic.Line({
+                        points: [X,Y, X+this.cellSide,Y, X+this.cellSide,Y+this.cellSide, X,Y+this.cellSide],
+                        fill: '#000000',
+                        fillPatternImage: this.errorPattern,
+                        stroke: this.frameColor,
+                        strokeWidth: this.frameLineWidth,
+                        closed: true,
+                        listening: true
+                    });
+
+                    //set up the tooltip listeners:
+                    this.cells[this.channelNames[i]].on('mouseover', this.writeTooltip.bind(this, i) );
+                    this.cells[this.channelNames[i]].on('mousemove', this.moveTooltip.bind(this) );
+                    this.cells[this.channelNames[i]].on('mouseout', this.writeTooltip.bind(this, -1));
+
+                    //set up onclick listeners:
+                    this.cells[this.channelNames[i]].on('click', this.clickCell.bind(this, this.channelNames[i]) );
+
+                    //add the cell to the main layer
+                    this.mainLayer.add(this.cells[this.channelNames[i]]);
+                }
+
+                //add the layers to the stage
+                this.stage.add(this.mainLayer);
+                this.stage.add(this.tooltipLayer);
+            }
+        }
+    });
+
+})();
--- xTags/detectors/detectorHelpers.js
+++ xTags/detectors/detectorHelpers.js
@@ -219,6 +219,255 @@ function initializeSingleViewDetector(name, channelNames, headline, URL){
     window.refreshTargets[window.refreshTargets.length] = this;
 }
 
+
+function initializeDetector(name, channelNames, headline, URL, viewNames){
+    var headWrapper = document.createElement('div')
+    ,   title = document.createElement('h1')
+    ,   viewTitles = ['HV', 'Threshold', 'Rate']
+    ,   drawTarget = document.createElement('div')
+    ,   plotControlWrap = document.createElement('form')
+    ,   plotControlTitle = document.createElement('h3')
+    ,   plotControlMinLabel = document.createElement('label')
+    ,   plotControlMaxLabel = document.createElement('label')
+    ,   plotControlMin = document.createElement('input')
+    ,   plotControlMax = document.createElement('input')
+    ,   plotScale = document.createElement('select')
+    ,   plotScaleLin = document.createElement('option')
+    ,   plotScaleLog = document.createElement('option')
+    ,   plotDeck = document.createElement('x-deck')
+    ,   plotCard = document.createElement('x-card')
+    ,   deckNavigator
+    //image has aspect ratio 3:2 and tries to be 80% of the window width, but not more than 80% of the window height
+    ,   width = this.offsetWidth
+    ,   height = 2*width/3
+    ,   i, subdetectorNav, subdetectorNavLabel
+
+    this.name = name;
+
+    //set up data store for detectors
+    if(!window.currentData)
+        window.currentData = {};
+    window.currentData.HV = {};
+    window.currentData.threshold = {};
+    window.currentData.rate = {};
+
+    //////////////////////
+    //Build DOM
+    //////////////////////
+    //the DOM layout for a detector view is roughly:
+
+    //-----------------------------------
+    // h1 Title
+    //-----------------------------------
+    // input[radio] HV / Thresh / Rate
+    //-----------------------------------
+    // x-deck for detector views; each
+    // card contains a Kinetic drawing
+    // context
+    //
+    //-----------------------------------
+    // x-deck navigation if required
+    //-----------------------------------
+    // form plot control widget
+    //-----------------------------------
+
+
+    headWrapper.setAttribute('id', this.id+'titleWrapper');
+    headWrapper.setAttribute('class', 'subdetectorHeadlineWrap')
+    this.appendChild(headWrapper);
+    //top nav title
+    title.setAttribute('id', this.id+'title');
+    title.setAttribute('class', 'subdetectorTitle');
+    document.getElementById(this.id+'titleWrapper').appendChild(title);
+    document.getElementById(this.id+'title').innerHTML = headline;
+    //state nav radio
+    for(i=0; i<viewTitles.length; i++){
+        subdetectorNav = document.createElement('input')
+        subdetectorNav.setAttribute('id', this.id+'goto'+viewTitles[i]);
+        subdetectorNav.setAttribute('class', 'subdetectorNavRadio');
+        subdetectorNav.setAttribute('type', 'radio');
+        subdetectorNav.setAttribute('name', this.id+'Nav');
+        subdetectorNav.setAttribute('value', viewTitles[i]);
+        subdetectorNav.onchange = this.trackView.bind(this);
+        if(i==2) subdetectorNav.setAttribute('checked', true); //default to rate view
+        document.getElementById(this.id+'titleWrapper').appendChild(subdetectorNav);
+        subdetectorNavLabel = document.createElement('label');
+        subdetectorNavLabel.setAttribute('id', this.id+'goto'+viewTitles[i]+'Label');
+        subdetectorNavLabel.setAttribute('class', 'subdetectorNavLabel');
+        subdetectorNavLabel.setAttribute('for', this.id+'goto'+viewTitles[i]);
+        document.getElementById(this.id+'titleWrapper').appendChild(subdetectorNavLabel);
+        document.getElementById(this.id+'goto'+viewTitles[i]+'Label').innerHTML = viewTitles[i];
+    }
+    //x-deck for views
+    plotDeck.setAttribute('id', this.id+'Deck');
+    plotDeck.setAttribute('selected-index', 0);
+    this.appendChild(plotDeck);
+
+    //plot buffers
+    for(i=0; i<viewNames.length; i++){
+        //x-cards for views
+        plotCard = document.createElement('x-card');
+        plotCard.setAttribute('id', this.id+viewNames[i]+'Card');
+        plotDeck.appendChild(plotCard);
+
+        //divs to hold kinetic contexts
+        drawTarget = document.createElement('div');
+        drawTarget.setAttribute('id', this.id+viewNames[i]+'Draw');
+        plotCard.appendChild(drawTarget);
+    }
+
+    //x-deck navigation
+    if(viewNames.length > 1){
+        deckNavigator = document.createElement('button');
+        deckNavigator.innerHTML = 'cycle deck';
+        var testID = this.id+'Deck';
+        deckNavigator.onclick = function(){
+            document.getElementById(testID).shuffleNext();
+        }
+    }
+
+    //plot control widget
+    plotControlWrap.setAttribute('id', this.id+'PlotControl');
+    plotControlWrap.setAttribute('class', 'plotControlWidget');
+    this.appendChild(plotControlWrap);
+    document.getElementById(this.id+'PlotControl').onchange = this.updatePlotParameters.bind(this);
+
+    plotControlTitle.setAttribute('id', this.id+'PlotControlTitle');
+    plotControlWrap.appendChild(plotControlTitle);
+    document.getElementById(this.id + 'PlotControlTitle').innerHTML = 'Scale Control'
+
+    plotControlMinLabel.setAttribute('id', this.id+'PlotControlMinLabel');
+    plotControlWrap.appendChild(plotControlMinLabel)
+    document.getElementById(this.id+'PlotControlMinLabel').innerHTML = 'Min: ';
+    plotControlMin.setAttribute('id', this.id + 'PlotControlMin');
+    plotControlMin.setAttribute('type', 'number');
+    plotControlMin.setAttribute('step', 'any');
+    plotControlWrap.appendChild(plotControlMin);
+
+    plotControlMaxLabel.setAttribute('id', this.id+'PlotControlMaxLabel');
+    plotControlWrap.appendChild(plotControlMaxLabel)    
+    document.getElementById(this.id+'PlotControlMaxLabel').innerHTML = 'Max: ';
+    plotControlMax.setAttribute('id', this.id + 'PlotControlMax');
+    plotControlMax.setAttribute('type', 'number');
+    plotControlMax.setAttribute('step', 'any');
+    plotControlWrap.appendChild(plotControlMax);
+
+    plotScale.setAttribute('id', this.id+'PlotControlScale');
+    plotControlWrap.appendChild(plotScale);
+
+    plotScaleLin.setAttribute('id', this.id+'PlotScaleLin');
+    plotScaleLin.setAttribute('value', 'lin');
+    plotScale.appendChild(plotScaleLin);
+    document.getElementById(this.id+'PlotScaleLin').innerHTML = 'Linear';
+
+    plotScaleLog.setAttribute('id', this.id+'PlotScaleLog');
+    plotScaleLog.setAttribute('value', 'log');
+    plotScale.appendChild(plotScaleLog);
+    document.getElementById(this.id+'PlotScaleLog').innerHTML = 'Log';
+
+    ///////////////////////
+    //State variables
+    ///////////////////////
+    this.currentView = 'Rate';
+    this.currentUnit = 'Hz';
+
+    ////////////////////////////
+    //Define Channels
+    ////////////////////////////
+    //declare the detector cell names for this detector:
+    this.channelNames = channelNames; //['DEMOCHAN00'];
+    this.cells = {};
+
+    ////////////////////////////
+    //Drawing parameters
+    ////////////////////////////
+    this.frameLineWidth = 2;
+    this.frameColor = '#999999';
+    this.width = width;
+    this.height = height;
+
+    ///////////////////////////
+    //Scale Parameters
+    ///////////////////////////
+    this.scale = 'ROOT Rainbow';
+    this.min = {HV: canHas(localStorage.getItem(name+'HVmin'), 0), 
+                Threshold: canHas(localStorage.getItem(name+'Thresholdmin'), 0), 
+                Rate: canHas(localStorage.getItem(name+'Ratemin'), 0)
+            };
+    this.max = {HV: canHas(localStorage.getItem(name+'HVmax'), 3000), 
+                Threshold: canHas(localStorage.getItem(name+'Thresholdmax'), 1000),
+                Rate: canHas(localStorage.getItem(name+'Ratemax'), 10000)
+            };
+    this.scaleType = {  HV: canHas(localStorage.getItem(name+'HVscaleType'), 'lin'),
+                        Threshold: canHas(localStorage.getItem(name+'ThresholdscaleType'), 'lin'), 
+                        Rate: canHas(localStorage.getItem(name+'RatescaleType'), 'lin')
+                    };
+
+    //if anything was in local storage, communicate this to the UI:
+    plotControlMin.value = this.min[this.currentView];
+    plotControlMax.value = this.max[this.currentView];
+    plotScale.value = this.scaleType[this.currentView];
+
+    ///////////////////////////
+    //Tooltip state
+    ///////////////////////////
+    this.lastTTindex = -1;
+
+    ////////////////////////////
+    //Kinetic.js setup
+    ////////////////////////////
+    //point kinetic at the div and set up the staging and layers:
+    this.stage = new Kinetic.Stage({
+        container: this.id+'Draw',
+        width: width,
+        height: height
+    });
+    this.mainLayer = new Kinetic.Layer();       //main rendering layer
+    this.tooltipLayer = new Kinetic.Layer();    //layer for tooltip info
+
+    //tooltip background:
+    this.TTbkg = new Kinetic.Rect({
+        x:60,
+        y:0,
+        width:100,
+        height:100,
+        fill:'rgba(0,0,0,0.8)',
+        stroke: 'rgba(0,0,0,0)',
+        listening: false
+    });
+    this.tooltipLayer.add(this.TTbkg);
+
+    //tooltip text:
+    this.text = new Kinetic.Text({
+        x: 70,
+        y: 10,
+        fontFamily: 'Arial',
+        fontSize: 16,
+        text: '',
+        lineHeight: 1.2,
+        fill: '#EEEEEE',
+        listening: false
+    });
+    this.tooltipLayer.add(this.text);
+
+    this.errorPattern = new Image();
+    this.errorPattern.src = 'static.gif'
+
+    //append data location information to list of URLs to fetch from:
+    if(!window.fetchURL)
+        window.fetchURL = [];
+    for(i=0; i<URL.length; i++){
+        if(URL[i] && window.fetchURL.indexOf(URL[i]) == -1){
+            window.fetchURL[window.fetchURL.length] = URL[i];
+        }
+    }
+    
+    //let repopulate know that the status bar would like to be updated every loop:
+    if(!window.refreshTargets)
+        window.refreshTargets = [];
+    window.refreshTargets[window.refreshTargets.length] = this;
+}
+
 //stick the ODB equipment directory into its local slot:
 function fetchODBEquipment(returnObj){
     if(!window.currentData.ODB)
