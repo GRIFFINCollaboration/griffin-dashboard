cc8acfc8838b40da3eb7534ff3485bb31cf35674
BillMills <mills.wj@gmail.com>
2014-04-14 16:13:14 -0700

    first stab at TIGRESS & multiview detector

--- build/combined.js
+++ build/combined.js
@@ -402,7 +402,7 @@ function initializeDetector(name, channelNames, headline, URL, viewNames){
     this.appendChild(deckWrap);
 
     //declaring x-tags from within other x-tags needs special treatment via innerHTML; must build HTML string and set it.
-    xString = '<x-deck id="' + this.id + 'Deck">';
+    xString = '<x-deck id="' + this.id + 'Deck" selected-index=0>';
 
     deckWrap.innerHTML = xString;
 /*
