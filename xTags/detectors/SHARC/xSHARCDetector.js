dc73fb6debb68c05dc594f1225d2907987fa1d60
BillMills <mills.wj@gmail.com>
2014-04-23 12:46:58 -0700

    SHARC summary

--- xTags/detectors/SHARC/xSHARCDetector.js
+++ xTags/detectors/SHARC/xSHARCDetector.js
@@ -86,14 +86,14 @@
                 cellCoords['SHQ15DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 0];
                 cellCoords['SHQ16DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 90];
 
-                cellCoords['SHB05DP'] = [{x: -0.75*this.width + this.grid, y: -0.4*this.height + this.long/2}, 'tall'];
-                cellCoords['SHB06DP'] = [{x: -0.75*this.width - this.grid*Math.cos(this.theta), y: -0.4*this.height + this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
-                cellCoords['SHB07DP'] = [{x: -0.75*this.width - this.grid, y: -0.4*this.height + this.long/2}, 'tall'];
-                cellCoords['SHB08DP'] = [{x: -0.75*this.width + this.grid*Math.cos(this.theta), y: -0.4*this.height - this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
-                cellCoords['SHB05DN'] = [{x: -0.75*this.width + 2*this.grid, y: -0.4*this.height + this.long/2}, 'tall'];
-                cellCoords['SHB06DN'] = [{x: -0.75*this.width - 2*this.grid*Math.cos(this.theta), y: -0.4*this.height + 2*this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
-                cellCoords['SHB07DN'] = [{x: -0.75*this.width - 2*this.grid, y: -0.4*this.height + this.long/2}, 'tall'];
-                cellCoords['SHB08DN'] = [{x: -0.75*this.width + 2*this.grid*Math.cos(this.theta), y: -0.4*this.height - 2*this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
+                cellCoords['SHB05DP'] = [{x: -0.75*this.width + this.grid, y: -0.6*this.height + this.long/2}, 'tall'];
+                cellCoords['SHB06DP'] = [{x: -0.75*this.width - this.grid*Math.cos(this.theta), y: -0.6*this.height + this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
+                cellCoords['SHB07DP'] = [{x: -0.75*this.width - this.grid, y: -0.6*this.height + this.long/2}, 'tall'];
+                cellCoords['SHB08DP'] = [{x: -0.75*this.width + this.grid*Math.cos(this.theta), y: -0.6*this.height - this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
+                cellCoords['SHB05DN'] = [{x: -0.75*this.width + 2*this.grid, y: -0.6*this.height + this.long/2}, 'tall'];
+                cellCoords['SHB06DN'] = [{x: -0.75*this.width - 2*this.grid*Math.cos(this.theta), y: -0.6*this.height + 2*this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
+                cellCoords['SHB07DN'] = [{x: -0.75*this.width - 2*this.grid, y: -0.6*this.height + this.long/2}, 'tall'];
+                cellCoords['SHB08DN'] = [{x: -0.75*this.width + 2*this.grid*Math.cos(this.theta), y: -0.6*this.height - 2*this.grid*Math.sin(this.theta) + this.short/2}, 'long'];
 
                 //upright and sideways parallelogram coords for SHB summaries
                 parallelogramCoords['tall'] = [0,this.short*Math.tan(this.theta), 0,this.long, this.short,this.long - this.short/Math.tan(this.theta), this.short,0];
@@ -142,7 +142,7 @@
                 }
 
                 var alignment = new Kinetic.Line({
-                    points:[0,0.4*this.height, this.width,0.4*this.height],
+                    points:[0,0.6*this.height, this.width,0.6*this.height],
                     stroke: '#FF0000'
                 })
                 this.mainLayer[0].add(alignment)
