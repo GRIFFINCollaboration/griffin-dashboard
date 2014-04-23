9f89a0c944f5a9941e6e044c4451b77625c5cc8a
BillMills <mills.wj@gmail.com>
2014-04-23 11:30:31 -0700

    SHARC summary

--- build/combined.js
+++ build/combined.js
@@ -16253,8 +16253,9 @@ function parseCustomPages(data){
                                         'SHB09f', 'SHB10f', 'SHB11f', 'SHB12f', 'SHQ13f', 'SHQ14f', 'SHQ15f', 'SHQ16f'
                                     ];
                 */
-                this.channelNames = [   'SHQ01b', 'SHQ02b', 'SHQ03b', 'SHQ04b', 'SHQ13b', 'SHQ14b', 'SHQ15b', 'SHQ16b',
-                                        'SHQ01f', 'SHQ02f', 'SHQ03f', 'SHQ04f', 'SHQ13f', 'SHQ14f', 'SHQ15f', 'SHQ16f'
+                this.channelNames = [   'SHQ01DN', 'SHQ02DN', 'SHQ03DN', 'SHQ04DN', 'SHQ13DN', 'SHQ14DN', 'SHQ15DN', 'SHQ16DN',
+                                        'SHQ01DP', 'SHQ02DP', 'SHQ03DP', 'SHQ04DP', 'SHQ13DP', 'SHQ14DP', 'SHQ15DP', 'SHQ16DP',
+                                        'SHB05DP'/*, 'SHB06DP', 'SHB07DP', 'SHB08DP', 'SHB05DN', 'SHB06DN', 'SHB07DN', 'SHB08DN'*/
                                     ];
 
                 initializeDetector.bind(this, 'SHARC', 'SHARC', URLs)();
@@ -16305,38 +16306,54 @@ function parseCustomPages(data){
                 //cell coords packed as: 
                 //SHQ***: [center X, center Y, rotation]
                 //SHB***: []
-                cellCoords['SHQ01b'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ02b'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ03b'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ04b'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 90];
-                cellCoords['SHQ13b'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ14b'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ15b'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ16b'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ01DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 180];
+                cellCoords['SHQ02DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), -90];
+                cellCoords['SHQ03DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 0];
+                cellCoords['SHQ04DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ13DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 180];
+                cellCoords['SHQ14DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), -90];
+                cellCoords['SHQ15DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 0];
+                cellCoords['SHQ16DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 90];
 
-                cellCoords['SHQ01f'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ02f'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ03f'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ04f'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 90];
-                cellCoords['SHQ13f'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ14f'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ15f'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ16f'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ01DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 180];
+                cellCoords['SHQ02DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), -90];
+                cellCoords['SHQ03DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 0];
+                cellCoords['SHQ04DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ13DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 180];
+                cellCoords['SHQ14DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), -90];
+                cellCoords['SHQ15DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 0];
+                cellCoords['SHQ16DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 90];
 
                 for(i=0; i<this.channelNames.length; i++){
-                    this.cells[this.channelNames[i]] = new Kinetic.Wedge({
-                        x: cellCoords[this.channelNames[i]][0],
-                        y: cellCoords[this.channelNames[i]][1],
-                        rotation: cellCoords[this.channelNames[i]][2],
-                        radius: this.rad,
-                        angle: 90,
-                        fill: '#000000',
-                        fillPatternImage: this.errorPattern,
-                        stroke: this.frameColor,
-                        strokeWidth: this.frameLineWidth,
-                        closed: true,
-                        listening: true               
-                    })
+                    //SHQ summaries
+                    if(i<16){
+                        this.cells[this.channelNames[i]] = new Kinetic.Wedge({
+                            x: cellCoords[this.channelNames[i]][0],
+                            y: cellCoords[this.channelNames[i]][1],
+                            rotation: cellCoords[this.channelNames[i]][2],
+                            radius: this.rad,
+                            angle: 90,
+                            fill: '#000000',
+                            fillPatternImage: this.errorPattern,
+                            stroke: this.frameColor,
+                            strokeWidth: this.frameLineWidth,
+                            closed: true,
+                            listening: true               
+                        })
+                    //SHB summaries
+                    } else if(i<32){
+                        this.cells[this.channelNames[i] = new Kinetic.Line({
+                            points: [0,this.short, this.short*Math.cos(this.theta),0, this.long, 0, this.long - this.short*Math.cos(this.theta), this.short],
+                            offset: 0,
+                            rotation: 0,
+                            fill: '#000000',
+                            fillPatternImage: this.errorPattern,
+                            stroke: this.frameColor,
+                            strokeWidth: this.frameLineWidth,
+                            closed: true,
+                            listening: true                            
+                        })
+                    }
 
                     //set up the tooltip listeners:
                     this.cells[this.channelNames[i]].on('mouseover', this.writeTooltip.bind(this, i) );
@@ -16350,44 +16367,6 @@ function parseCustomPages(data){
                     this.mainLayer[0].add(this.cells[this.channelNames[i]]);
                 }
 
-
-
-                /*
-                for(i=0; i<10; i++){ //10 rings
-                    for(j=0; j<12; j++){ //12 phi sectors
-
-                        this.cells[this.channelNames[chan]] = new Kinetic.Arc({
-                            innerRadius: this.innerRad + i*this.radStep,
-                            outerRadius: this.innerRad + (i+1)*this.radStep,
-                            fill: '#000000',
-                            fillPatternImage: this.errorPattern,
-                            stroke: this.frameColor,
-                            strokeWidth: this.frameLineWidth,
-                            angle: 30,
-                            clockwise: false,
-                            rotationDeg: -30*(j+1),
-                            x: this.x0,
-                            y: this.y0,
-                            closed: true,
-                            listening: true
-                        });
-
-                        //set up the tooltip listeners:
-                        this.cells[this.channelNames[chan]].on('mouseover', this.writeTooltip.bind(this, chan) );
-                        this.cells[this.channelNames[chan]].on('mousemove', this.moveTooltip.bind(this) );
-                        this.cells[this.channelNames[chan]].on('mouseout', this.writeTooltip.bind(this, -1));
-
-                        //set up onclick listeners:
-                        this.cells[this.channelNames[chan]].on('click', this.clickCell.bind(this, this.channelNames[chan]) );
-
-                        //add the cell to the main layer
-                        this.mainLayer[0].add(this.cells[this.channelNames[chan]]);
-
-                        chan++;
-                    }
-
-                }
-                */
                 //add the layers to the stage
                 this.stage[0].add(this.mainLayer[0]);
                 this.stage[0].add(this.tooltipLayer[0]);
