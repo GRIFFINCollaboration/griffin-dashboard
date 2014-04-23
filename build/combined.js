3f117cd3572d46f18dbfe86ee561656280eb8dcb
BillMills <mills.wj@gmail.com>
2014-04-23 11:36:14 -0700

    SHARC summary

--- build/combined.js
+++ build/combined.js
@@ -16345,7 +16345,7 @@ function parseCustomPages(data){
                         this.cells[this.channelNames[i]] = new Kinetic.Line({
                             points: [0,this.short, this.short*Math.cos(this.theta),0, this.long, 0, this.long - this.short*Math.cos(this.theta), this.short],
                             offset: 0,
-                            rotation: 0,
+                            rotation: 90,
                             fill: '#000000',
                             fillPatternImage: this.errorPattern,
                             stroke: this.frameColor,
