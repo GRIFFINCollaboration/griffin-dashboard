0aac6c453675c2dcbbab04da2389041ca10e07fc
BillMills <mills.wj@gmail.com>
2014-04-23 13:17:14 -0700

    SHARC summary

--- xTags/detectors/SHARC/xSHARCDetector.js
+++ xTags/detectors/SHARC/xSHARCDetector.js
@@ -60,40 +60,50 @@
         methods: {
             'instantiateCells': function(){
                 var i, 
-                    cellCoords = {};
-                    parallelogramCoords = {}
+                    cellCoords = {},
+                    parallelogramCoords = {},
+                    downstreamBoxCenterX = -0.625*this.width,
+                    downstreamBoxCenterY = -0.8*this.height + this.width/8*Math.tan(this.theta) + this.long/2,
+                    downstreamSHQfrontCenterX = 0.75*this.width + 3*this.grid*Math.cos(this.theta),
+                    downstreamSHQfrontCenterY = 0.4*this.height - 3*this.grid*Math.sin(this.theta),
+                    downstreamSHQbackCenterX = 0.75*this.width + 4*this.grid*Math.cos(this.theta),
+                    downstreamSHQbackCenterY = 0.4*this.height - 4*this.grid*Math.sin(this.theta),
+                    upstreamSHQfrontCenterX = 0.25*this.width - 3*this.grid*Math.cos(this.theta),
+                    upstreamSHQfrontCenterY = 0.4*this.height + 3*this.grid*Math.sin(this.theta),
+                    upstreamSHQbackCenterX = 0.25*this.width - 4*this.grid*Math.cos(this.theta),
+                    upstreamSHQbackCenterY = 0.4*this.height + 4*this.grid*Math.sin(this.theta)
 
                 //each channel listed in this.channelNames gets an entry in this.cells as a Kinetic object:
                 //summary layout
                 //cell coords packed as: 
                 //SHQ***: [center X, center Y, rotation]
                 //SHB***: [{offsetX, offsetY}, orientation] - note Kinetic offsets 'move the viewport'; ie -100 offset in x moves the shape 100px *right* 
-                cellCoords['SHQ01DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ02DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ03DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ04DN'] = [0.75*this.width + 4*this.grid*Math.cos(this.theta), 0.4*this.height - 4*this.grid*Math.sin(this.theta), 90];
-                cellCoords['SHQ13DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ14DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ15DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ16DN'] = [0.25*this.width - 4*this.grid*Math.cos(this.theta), 0.4*this.height + 4*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ01DN'] = [downstreamSHQbackCenterX, downstreamSHQbackCenterY, 180];
+                cellCoords['SHQ02DN'] = [downstreamSHQbackCenterX, downstreamSHQbackCenterY, -90];
+                cellCoords['SHQ03DN'] = [downstreamSHQbackCenterX, downstreamSHQbackCenterY, 0];
+                cellCoords['SHQ04DN'] = [downstreamSHQbackCenterX, downstreamSHQbackCenterY, 90];
+                cellCoords['SHQ13DN'] = [upstreamSHQbackCenterX, upstreamSHQbackCenterY, 180];
+                cellCoords['SHQ14DN'] = [upstreamSHQbackCenterX, upstreamSHQbackCenterY, -90];
+                cellCoords['SHQ15DN'] = [upstreamSHQbackCenterX, upstreamSHQbackCenterY, 0];
+                cellCoords['SHQ16DN'] = [upstreamSHQbackCenterX, upstreamSHQbackCenterY, 90];
 
-                cellCoords['SHQ01DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ02DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ03DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ04DP'] = [0.75*this.width + 3*this.grid*Math.cos(this.theta), 0.4*this.height - 3*this.grid*Math.sin(this.theta), 90];
-                cellCoords['SHQ13DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 180];
-                cellCoords['SHQ14DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), -90];
-                cellCoords['SHQ15DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 0];
-                cellCoords['SHQ16DP'] = [0.25*this.width - 3*this.grid*Math.cos(this.theta), 0.4*this.height + 3*this.grid*Math.sin(this.theta), 90];
+                cellCoords['SHQ01DP'] = [downstreamSHQfrontCenterX, downstreamSHQfrontCenterY, 180];
+                cellCoords['SHQ02DP'] = [downstreamSHQfrontCenterX, downstreamSHQfrontCenterY, -90];
+                cellCoords['SHQ03DP'] = [downstreamSHQfrontCenterX, downstreamSHQfrontCenterY, 0];
+                cellCoords['SHQ04DP'] = [downstreamSHQfrontCenterX, downstreamSHQfrontCenterY, 90];
+                cellCoords['SHQ13DP'] = [upstreamSHQfrontCenterX, upstreamSHQfrontCenterY, 180];
+                cellCoords['SHQ14DP'] = [upstreamSHQfrontCenterX, upstreamSHQfrontCenterY, -90];
+                cellCoords['SHQ15DP'] = [upstreamSHQfrontCenterX, upstreamSHQfrontCenterY, 0];
+                cellCoords['SHQ16DP'] = [upstreamSHQfrontCenterX, upstreamSHQfrontCenterY, 90];
 
-                cellCoords['SHB05DP'] = [{x: -0.625*this.width + this.grid, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + this.long/2}, 'tall'];
-                cellCoords['SHB06DP'] = [{x: -0.625*this.width, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + this.grid + this.short/2}, 'long'];
-                cellCoords['SHB07DP'] = [{x: -0.625*this.width - this.grid, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + this.long/2}, 'tall'];
-                cellCoords['SHB08DP'] = [{x: -0.625*this.width, y: -0.8*this.height + this.width/8*Math.tan(this.theta) - this.grid + this.short/2}, 'long'];
-                cellCoords['SHB05DN'] = [{x: -0.625*this.width + 2*this.grid, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + this.long/2}, 'tall'];
-                cellCoords['SHB06DN'] = [{x: -0.625*this.width, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + 2*this.grid + this.short/2}, 'long'];
-                cellCoords['SHB07DN'] = [{x: -0.625*this.width - 2*this.grid, y: -0.8*this.height + this.width/8*Math.tan(this.theta) + this.long/2}, 'tall'];
-                cellCoords['SHB08DN'] = [{x: -0.625*this.width, y: -0.8*this.height + this.width/8*Math.tan(this.theta) - 2*this.grid + this.short/2}, 'long'];
+                cellCoords['SHB05DP'] = [{x: downstreamBoxCenterX + this.grid, y: downstreamBoxCenterY + this.long/2}, 'tall'];
+                cellCoords['SHB06DP'] = [{x: downstreamBoxCenterX, y: downstreamBoxCenterY + this.grid + this.short/2}, 'long'];
+                cellCoords['SHB07DP'] = [{x: downstreamBoxCenterX - this.grid, y: downstreamBoxCenterY + this.long/2}, 'tall'];
+                cellCoords['SHB08DP'] = [{x: downstreamBoxCenterX, y: downstreamBoxCenterY - this.grid + this.short/2}, 'long'];
+                cellCoords['SHB05DN'] = [{x: downstreamBoxCenterX + 2*this.grid, y: downstreamBoxCenterY + this.long/2}, 'tall'];
+                cellCoords['SHB06DN'] = [{x: downstreamBoxCenterX, y: downstreamBoxCenterY + 2*this.grid + this.short/2}, 'long'];
+                cellCoords['SHB07DN'] = [{x: downstreamBoxCenterX - 2*this.grid, y: downstreamBoxCenterY + this.long/2}, 'tall'];
+                cellCoords['SHB08DN'] = [{x: downstreamBoxCenterX, y: downstreamBoxCenterY - 2*this.grid + this.short/2}, 'long'];
 
                 //upright and sideways parallelogram coords for SHB summaries
                 parallelogramCoords['tall'] = [0,this.short*Math.tan(this.theta), 0,this.long, this.short,this.long - this.short/Math.tan(this.theta), this.short,0];
