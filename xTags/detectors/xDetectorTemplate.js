a6570831817f3526d30f1fad7255d7696fa2948b
BillMills <mills.wj@gmail.com>
2014-04-24 15:39:57 -0700

    FF CSS

--- xTags/detectors/xDetectorTemplate.js
+++ xTags/detectors/xDetectorTemplate.js
@@ -240,13 +240,6 @@
                 document.getElementById(this.id + 'PlotControlMin').setAttribute('value', this.min[this.currentView]);
                 document.getElementById(this.id + 'PlotControlMax').setAttribute('value', this.max[this.currentView]);
 
-                //fake test data
-                window.currentData.rate.TIS01GN01X = 100
-                window.currentData.rate.TIS01GN02X = 200
-                window.currentData.rate.TIS01GN03X = 300
-                window.currentData.rate.TIS01GN04X = 400
-                window.currentData.rate.TIS01GN05X = 500
-
                 //sort data into summary statistics if necessary
                 if(this.summaryDepth)
                     this.summarizeData();
