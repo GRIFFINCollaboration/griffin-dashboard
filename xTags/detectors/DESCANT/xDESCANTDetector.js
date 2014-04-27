6bad3d9eb7a80516ea9815b607f84d3fe4e77709
BillMills <mills.wj@gmail.com>
2014-04-26 20:41:14 -0700

    messing around with DESCANT cells

--- xTags/detectors/DESCANT/xDESCANTDetector.js
+++ xTags/detectors/DESCANT/xDESCANTDetector.js
@@ -12,7 +12,7 @@
                 //deploy the standard stuff
                 this.viewNames = ['SingleView'];
                 //channels start at top left hand corner and walk across in rows
-                this.channelNames = ['test0', 'test1', 'test2']
+                this.channelNames = ['test0', 'test1', 'test2', 'test3']
                 initializeDetector.bind(this, 'DESCANT', 'DESCANT', URLs)();
 
                 //////////////////////////////////////
@@ -63,7 +63,8 @@
                     cellOrder = ['white', 'white', 'white', 'white', 'greenLeft', 'greenLeft', 'greenRight', 'greenRight', 'red', 'red', 'red', 'blue', 'blue', 'blue'],
                     baseCoords = [  {'x':-this.width/2, 'y':-0.4*this.height+pentagonNormal+71.9*scale},
                                     {'x':-this.width/2, 'y':-0.4*this.height+pentagonNormal+(223.4 + explode/0.4)*scale},
-                                    {'x':-this.width/2, 'y':-0.4*this.height+pentagonNormal+(374.9 + 2*explode/0.4)*scale}
+                                    {'x':-this.width/2, 'y':-0.4*this.height+pentagonNormal+(374.9 + 2*explode/0.4)*scale},
+                                    {'x':-this.width/2, 'y':-0.4*this.height+pentagonNormal+(526.4 + 3*explode/0.4)*scale}
                                     ],
                     internalRotation = [0];
 
