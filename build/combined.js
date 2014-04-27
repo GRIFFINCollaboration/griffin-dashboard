8fd84aef47ca659efb8de0850ce57dd1ff5651fc
BillMills <mills.wj@gmail.com>
2014-04-27 00:20:41 -0700

    messing around with DESCANT cells

--- build/combined.js
+++ build/combined.js
@@ -16227,7 +16227,7 @@ var Kinetic = {};
                 //deploy the standard stuff
                 this.viewNames = ['SingleView'];
                 //channels start at top left hand corner and walk across in rows
-                this.channelNames = ['test0', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11']
+                this.channelNames = ['test0', 'test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11', 'test12']
                 initializeDetector.bind(this, 'DESCANT', 'DESCANT', URLs)();
 
                 //////////////////////////////////////
@@ -16281,12 +16281,13 @@ var Kinetic = {};
                                     [Math.sin(-22/180*Math.PI)*4.8*grid + this.width/2, -Math.cos(-22/180*Math.PI)*4.8*grid + 0.4*this.height],
 
                                     [Math.sin(30/180*Math.PI)*1.7*grid + this.width/2, -Math.cos(30/180*Math.PI)*1.7*grid + 0.4*this.height],
-                                    [Math.sin(15/180*Math.PI)*3.5*grid + this.width/2, -Math.cos(15/180*Math.PI)*3.5*grid + 0.4*this.height],
+                                    [Math.sin(60/180*Math.PI)*3.5*grid + this.width/2, -Math.cos(60/180*Math.PI)*3.5*grid + 0.4*this.height],
                                     [Math.sin(60/180*Math.PI)*3.5*grid + this.width/2, -Math.cos(60/180*Math.PI)*3.5*grid + 0.4*this.height],
 
-                                    [Math.sin(20/180*Math.PI)*2.7*grid + this.width/2, -Math.cos(20/180*Math.PI)*2.7*grid + 0.4*this.height]
+                                    [Math.sin(20/180*Math.PI)*2.7*grid + this.width/2, -Math.cos(20/180*Math.PI)*2.7*grid + 0.4*this.height],
+                                    [Math.sin(30/180*Math.PI)*2.7*grid + this.width/2, -Math.cos(30/180*Math.PI)*2.7*grid + 0.4*this.height]
                                 ],
-                    internalRotation = [0,0,0,0,-78,-72,78,72,120,130,130,0];
+                    internalRotation = [0,0,0,0,-78,-72,78,72,120,130,130,0,0];
 
 
                 //each channel listed in this.channelNames gets an entry in this.cells as a Kinetic object:
