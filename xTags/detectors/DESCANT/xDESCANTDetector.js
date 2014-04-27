722b91d9fa19d47fd79e83c7b5b0c6d0b07d04d9
BillMills <mills.wj@gmail.com>
2014-04-27 00:01:58 -0700

    messing around with DESCANT cells

--- xTags/detectors/DESCANT/xDESCANTDetector.js
+++ xTags/detectors/DESCANT/xDESCANTDetector.js
@@ -64,7 +64,7 @@
                                     [Math.sin(-10/180*Math.PI)*4.8*grid + this.width/2, -Math.cos(-10/180*Math.PI)*4.8*grid + 0.4*this.height],
                                     [Math.sin(-22/180*Math.PI)*4.8*grid + this.width/2, -Math.cos(-22/180*Math.PI)*4.8*grid + 0.4*this.height],
                                     [Math.sin(30/180*Math.PI)*1.7*grid + this.width/2, -Math.cos(30/180*Math.PI)*1.7*grid + 0.4*this.height],
-                                    [Math.sin(17/180*Math.PI)*3.5*grid + this.width/2, -Math.cos(17/180*Math.PI)*3.5*grid + 0.4*this.height]
+                                    [Math.sin(15/180*Math.PI)*3.5*grid + this.width/2, -Math.cos(15/180*Math.PI)*3.5*grid + 0.4*this.height]
                                 ],
                     internalRotation = [0,0,0,0,-78,-72,78,72,120,120];
 
