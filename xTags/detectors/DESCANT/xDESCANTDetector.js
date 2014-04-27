9a50709c33eead7a1a7addb33bee99ff19631a77
BillMills <mills.wj@gmail.com>
2014-04-26 23:30:06 -0700

    messing around with DESCANT cells

--- xTags/detectors/DESCANT/xDESCANTDetector.js
+++ xTags/detectors/DESCANT/xDESCANTDetector.js
@@ -64,7 +64,7 @@
                                     [Math.sin(-10/180*Math.PI)*4.8*grid + this.width/2, -Math.cos(-10/180*Math.PI)*4.8*grid + 0.4*this.height],
                                     [Math.sin(-22/180*Math.PI)*4.8*grid + this.width/2, -Math.cos(-22/180*Math.PI)*4.8*grid + 0.4*this.height]
                                 ],
-                    internalRotation = [0,0,0,0,-78,-78,102,102];
+                    internalRotation = [0,0,0,0,-78,-72,-102,-108];
 
 
                 //each channel listed in this.channelNames gets an entry in this.cells as a Kinetic object:
