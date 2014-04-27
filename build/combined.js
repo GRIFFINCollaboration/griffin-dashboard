b6cabaeab39ac22ae44379dc0d2a810f38ed3283
BillMills <mills.wj@gmail.com>
2014-04-26 23:21:26 -0700

    messing around with DESCANT cells

--- build/combined.js
+++ build/combined.js
@@ -16274,7 +16274,7 @@ var Kinetic = {};
                                     [this.width/2, 0.4*this.height-2*grid],
                                     [this.width/2, 0.4*this.height-3*grid],
                                     [this.width/2, 0.4*this.height-4*grid],
-                                    [Math.sin(12/180*Math.PI)*5*grid + this.width/2, -Math.cos(12/180*Math.PI)*5*grid + 0.4*this.height]
+                                    [Math.sin(12/180*Math.PI)*4.5*grid + this.width/2, -Math.cos(12/180*Math.PI)*4.5*grid + 0.4*this.height]
                                 ],
                     internalRotation = [0,0,0,0,-78];
 
