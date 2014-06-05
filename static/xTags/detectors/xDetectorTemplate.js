872d7c1551f698e0a0bdd6807123c16446f62c66
BillMills <mills.wj@gmail.com>
2014-06-05 14:10:53 -0700

    constructing hostmap for DAQ node querying

--- static/xTags/detectors/xDetectorTemplate.js
+++ static/xTags/detectors/xDetectorTemplate.js
@@ -267,6 +267,7 @@
 
             'update': function(){
                 //trigger a new round of data fetching
+console.log(window.currentData.MSC)
                 this.acquireRates();
                 this.acquireThresholds();
                 this.acquireHV();                
