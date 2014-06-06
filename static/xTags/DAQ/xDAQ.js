c733811c7c86e111e4f7dbb07beb8279555317c9
BillMills <mills.wj@gmail.com>
2014-06-06 16:58:25 -0700

    began DAQ monitor

---
+++ static/xTags/DAQ/xDAQ.js
@@ -0,0 +1,26 @@
+(function(){  
+
+    xtag.register('widget-DAQ', {
+        extends: 'div',
+        lifecycle: {
+            created: function() {
+                XHR(this.midas + '/?cmd=jcopy&odb=/DAQ&encoding=json-nokeys', function(res){console.log(JSON.parse(res))}, 'application/json')
+            },
+            inserted: function() {},
+            removed: function() {},
+            attributeChanged: function() {}
+        }, 
+        events: { 
+
+        },
+        accessors: {
+            'MIDAS':{
+                attribute: {} //this just needs to be declared
+            }
+        }, 
+        methods: {
+  
+        }
+    });
+
+})();
