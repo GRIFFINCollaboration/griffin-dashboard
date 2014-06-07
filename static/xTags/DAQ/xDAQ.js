112bc18062df98d827d4ebb34720537570719669
BillMills <mills.wj@gmail.com>
2014-06-06 18:11:51 -0700

    building bones of DAQ DOM

--- static/xTags/DAQ/xDAQ.js
+++ static/xTags/DAQ/xDAQ.js
@@ -4,7 +4,26 @@
         extends: 'div',
         lifecycle: {
             created: function() {
-                XHR('http://' + this.MIDAS + '/?cmd=jcopy&odb=/DAQ&encoding=json-nokeys', function(res){console.log(res) }, 'application/json')
+                var xString;
+
+                //get the DAQ structure
+                XHR('http://' + this.MIDAS + '/?cmd=jcopy&odb=/DAQ&encoding=json-nokeys', 
+                    function(res){
+                        console.log(res) 
+                    }, 
+                    'application/json');
+
+                xString = '<x-deck id="' + this.id + 'Deck" selected-index=0>';
+                xString += '<x-card id="DAQmasterCard"></x-card></x-deck>';
+                xtag.innerHTML(this, xString);
+
+                this.masterBlock = document.createElement('div');
+                this.masterBlock.setAttribute('class', 'masterDAQ');
+                document.getElementById('DAQmasterCard').appendChild(this.masterBlock);
+
+                this.collectorBlock = document.createElement('div');
+                this.collectorBlock.setAttribute('class', 'collectorDAQ');
+                document.getElementById('DAQmasterCard').appendChild(this.collectorBlock)
             },
             inserted: function() {},
             removed: function() {},
