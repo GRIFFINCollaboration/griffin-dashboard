20567fd076b9a74244f4577d9f8ccd5c95a3068f
BillMills <mills.wj@gmail.com>
2014-06-11 15:10:27 -0700

    registering new cycle in ODB

--- routes.js
+++ routes.js
@@ -60,6 +60,7 @@ app.post('/registerCycle', function(req, res){
 	spawn('odbedit', ['-c', "mkdir /PPG/Cycles/" + req.body.cycleName]);
 	spawn('odbedit', ['-c', "create int /PPG/Cycles/" + req.body.cycleName + "/PPGcodes[" + steps.length + "]"]);
 	spawn('odbedit', ['-c', "create int /PPG/Cycles/" + req.body.cycleName + "/durations[" + steps.length + "]"]);
+	spawn('odbedit', ['-c', "set /PPG/Cycles/" + req.body.cycleName + "/PPGcodes " + steps);
 
 	return res.redirect('/PPG');
 });
