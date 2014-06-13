47fbc6a10537dd8278343c46f45e3e94dcf508fa
BillMills <mills.wj@gmail.com>
2014-06-13 16:35:22 -0700

    cycle detail toggle

--- static/xTags/PPG/xPPG.js
+++ static/xTags/PPG/xPPG.js
@@ -229,9 +229,7 @@
                     timingUI = ribbonCards[j].querySelectorAll('div#timingWrap');
                     durationSummary = ribbonCards[j].querySelectorAll('span#durationSummary');
                     duration = parseFloat(ribbonCards[j].querySelectorAll('input[type="number"]')[0].value);
-                    durationScale = parseInt(selected(ribbonCards[j].querySelectorAll('select')[0].id),10);   
-                    list = ribbonCards[j].querySelectorAll('ul'),
-                    listItems = list[0].querySelectorAll('li input[type="checkbox"]:checked');            
+                    durationScale = parseInt(selected(ribbonCards[j].querySelectorAll('select')[0].id),10);            
 
                     if(currentState == 'Edit'){
                         for(i=0; i<checkboxes.length; i++){
@@ -239,9 +237,7 @@
                         }
                         timingUI[0].setAttribute('class', 'edit');
                         durationSummary[0].setAttribute('class', 'edit');
-                        for(k=0; k<listItems.length; k++){
-                            listItems[k].setAttribute('class', '');
-                        }
+
                         document.getElementById('cycleDefinitionForm').setAttribute('class', 'PPGcontrol edit');
                     } else {
                         for(i=0; i<checkboxes.length; i++){
@@ -249,9 +245,6 @@
                         }
                         timingUI[0].setAttribute('class', 'summary');
                         durationSummary[0].setAttribute('class', 'summary');
-                        for(k=0; k<listItems.length; k++){
-                            listItems[k].setAttribute('class', 'summary');
-                        }
 
                         if(durationScale == 60000){
                             durationSummary[0].innerHTML = duration + ' min';
