54c1ed41c5d517d31a6bc7153a0a15c0de3086b3
BillMills <mills.wj@gmail.com>
2014-06-25 16:24:32 -0700

    working out filter commit route

--- routes.js
+++ routes.js
@@ -127,6 +127,10 @@ app.post('/registerFilter', function(req, res){
 	spawn('odbedit', ['-c', "rm /Filter/Filters/" + req.body.filterName]);
 
 	spawn('odbedit', ['-c', "mkdir /Filter/Filters/" + req.body.filterName]);
+	for(i=0; i<filter.length; i++){
+		spawn('odbedit', ['-c', "create string /Filter/Filters/" + req.body.filterName + "/orCondition "+i+"[" + filter[i].length + "]"]);	
+	}
+
 	/*
 	spawn('odbedit', ['-c', "create int /PPG/Cycles/" + req.body.cycleName + "/PPGcodes[" + steps.length + "]"]);
 	spawn('odbedit', ['-c', "create int /PPG/Cycles/" + req.body.cycleName + "/durations[" + steps.length + "]"]);
