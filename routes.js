806da5de036bbb6dbb3251eee1cf12f3dc9edf25
BillMills <mills.wj@gmail.com>
2014-06-26 11:31:36 -0700

    setting current filter in ODB

--- routes.js
+++ routes.js
@@ -104,7 +104,7 @@ app.post('/registerFilter', function(req, res){
 		i, j,
 		steps = [],
 		durations = [];
-console.log(filter)
+console.log(req.body.filterString)
 /*
 	//just load an existing cycle
 	if(req.body.loadTarget != 'null'){
