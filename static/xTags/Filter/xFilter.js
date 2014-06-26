a06679656fb1bdcab90b84e9ca66a735bb6b97a8
BillMills <mills.wj@gmail.com>
2014-06-26 12:20:44 -0700

    messing around with regex

--- static/xTags/Filter/xFilter.js
+++ static/xTags/Filter/xFilter.js
@@ -72,7 +72,7 @@
                 filterName.setAttribute('type', 'text');
                 filterName.setAttribute('id', 'filterName');
                 filterName.setAttribute('name', 'filterName');
-                filterName.setAttribute('pattern',"\S/g");
+                filterName.setAttribute('pattern', '^[\S]*$');
                 filterName.setAttribute('required', true);
                 controlRows[0].appendChild(filterNameLabel);
                 controlRows[0].appendChild(filterName);
