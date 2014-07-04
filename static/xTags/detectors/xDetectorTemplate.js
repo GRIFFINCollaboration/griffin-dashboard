dc08aace612ffb82c558563c78cfa07a971b9914
BillMills <mills.wj@gmail.com>
2014-07-04 11:54:41 -0700

    new tooltips for HV - offset bug

--- static/xTags/detectors/xDetectorTemplate.js
+++ static/xTags/detectors/xDetectorTemplate.js
@@ -126,8 +126,10 @@
                     left = mousePos.x,
                     top = mousePos.y,
                     element = this;
-
+                console.log('offset coords')
                 do{
+                    console.log(element.id)
+                    console.log([element.offsetLeft || 0, element.offsetTop || 0])
                     offsetTop += element.offsetTop || 0;
                     offsetLeft += element.offsetLeft || 0;
                     element = element.offsetParent;
