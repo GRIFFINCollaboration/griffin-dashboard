ffe83673de7c6fe161be8b8809fe2613d527c636
BillMills <mills.wj@gmail.com>
2014-07-04 14:24:41 -0700

    smart linking for nav bar

--- static/xTags/nav/xNav.js
+++ static/xTags/nav/xNav.js
@@ -5,24 +5,32 @@
         extends: 'div',
         lifecycle: {
             created: function() {
-                var baseURL = 'http://'+window.location.host,
-                    link,
-                    routes = ['HV', 'GRIFFIN', 'DAQ', 'PPG', 'Clocks', 'Filter'],
-                    present = (window.location+'').slice( (window.location+'').lastIndexOf('/') + 1),
-                    i;
 
-                    for(i=0; i<routes.length; i++){
-                        link = document.createElement('a')
-                        link.setAttribute('href', '/'+routes[i]);
-                        if(routes[i] == present)
-                            link.setAttribute('class', 'stdin present');
-                        else
-                            link.setAttribute('class', 'stdin');
-                        link.innerHTML = routes[i];
-                        this.appendChild(link);
-                    }
+                this.baseURL = 'http://'+window.location.host;
+                this.routes = ['HV', 'DAQ', 'PPG', 'Clocks', 'Filter'];
+                this.present = (window.location+'').slice( (window.location+'').lastIndexOf('/') + 1);
 
+                //get the DAQ structure, use it to decide which detectors need to be linked
+                XHR('http://' + this.MIDAS + '/?cmd=jcopy&odb=/DAQ&encoding=json-nokeys', 
+                    function(res){
+                        var MSC = JSON.parse(res),
+                            i, link;
 
+                        this.traverseMSC(MSC);
+
+                        for(i=0; i<this.routes.length; i++){
+                            link = document.createElement('a')
+                            link.setAttribute('href', '/'+this.routes[i]);
+                            if(this.routes[i] == this.present)
+                                link.setAttribute('class', 'stdin present');
+                            else
+                                link.setAttribute('class', 'stdin');
+                            link.innerHTML = this.routes[i];
+                            this.appendChild(link);
+                        }
+                        
+                    }.bind(this), 
+                    'application/json');
             },
             inserted: function() {},
             removed: function() {},
@@ -37,26 +45,42 @@
             }
         }, 
         methods: {
-            //deploy buttons; <customPages> == array of names of custom pages
-            'setup': function(customPages){
-                var i, link;
+            //examine the MSC table, and insert any detector found there into the list of things to link to
+            'traverseMSC' : function(MSC){
+                var i, detCode;
 
-                //always link to the status page first
-                link = document.createElement('a');
-                link.href = 'http://'+window.location.host;
-                link.innerHTML = 'Status';
-                this.appendChild(link);
-                link.setAttribute('class', 'linkRow');                
+                for(i=0; i<MSC.chan.length; i++){
+                    detCode = MSC.chan[i].slice(0,2);
 
-                //link to all the custom pages, except this one
-                for(i=0; i<customPages.length; i++){
-                    if('http://'+window.location.host + '/CS/' + customPages[i] == window.location)
-                        continue;
-                    link = document.createElement('a');
-                    link.href = 'http://'+window.location.host + '/CS/' + customPages[i];
-                    link.innerHTML = customPages[i];
-                    this.appendChild(link);
-                    link.setAttribute('class', 'linkRow');
+                    if(detCode == 'GR' && this.routes.indexOf('GRIFFIN')==-1 )
+                        this.routes.push('GRIFFIN');
+
+                    if(detCode == 'TI' && this.routes.indexOf('TIGRESS')==-1 )
+                        this.routes.push('TIGRESS');
+
+                    if(detCode == 'BA' && this.routes.indexOf('BAMBINO')==-1 )
+                        this.routes.push('BAMBINO');
+
+                    if(detCode == 'SH' && this.routes.indexOf('SHARC')==-1 )
+                        this.routes.push('SHARC');
+
+                    if(detCode == 'SP' && this.routes.indexOf('SPICE')==-1 )
+                        this.routes.push('SPICE');
+
+                    if(detCode == 'DS' && this.routes.indexOf('DESCANT')==-1 )
+                        this.routes.push('DESCANT');
+
+                    if(detCode == 'SE' && this.routes.indexOf('SCEPTAR')==-1 )
+                        this.routes.push('SCEPTAR');
+
+                    if(detCode == 'PA' && this.routes.indexOf('PACES')==-1 )
+                        this.routes.push('PACES');
+
+                    if(detCode == 'TPC' && this.routes.indexOf('TIPBall')==-1 )
+                        this.routes.push('TIPBall');
+
+                    if(detCode == 'TPW' && this.routes.indexOf('TIPWall')==-1 )
+                        this.routes.push('TIPWall');
                 }
             }
         }
