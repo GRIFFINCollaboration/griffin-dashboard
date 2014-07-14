d1c49777ec9258555580f8631bb8f609bfa62ecb
BillMills <mills.wj@gmail.com>
2014-07-14 10:06:37 -0700

    building DAQ bar graphs

--- static/xTags/DAQ/xDAQ.js
+++ static/xTags/DAQ/xDAQ.js
@@ -376,6 +376,59 @@
                 //babysit x-deck height
                 document.getElementById('DAQdeck').setAttribute('style', 'height:' + (document.getElementById('collectorBlock').offsetHeight + document.getElementById('masterHeadNode').offsetHeight) + 'px;')
 
+            (function basic_bars(container, horizontal) {
+
+                var
+                horizontal = (horizontal ? true : false),
+                    // Show horizontal bars
+                    d1 = [],
+                    // First data series
+                    d2 = [],
+                    // Second data series
+                    point, // Data point variable declaration
+                    i;
+
+                for (i = 0; i < 4; i++) {
+
+                    if (horizontal) {
+                        point = [Math.ceil(Math.random() * 10), i];
+                    } else {
+                        point = [i, Math.ceil(Math.random() * 10)];
+                    }
+
+                    d1.push(point);
+
+                    if (horizontal) {
+                        point = [Math.ceil(Math.random() * 10), i + 0.5];
+                    } else {
+                        point = [i + 0.5, Math.ceil(Math.random() * 10)];
+                    }
+
+                    d2.push(point);
+                };
+
+                // Draw the graph
+                Flotr.draw(
+                container, [d1, d2], {
+                    bars: {
+                        show: true,
+                        horizontal: horizontal,
+                        shadowSize: 0,
+                        barWidth: 0.5
+                    },
+                    mouse: {
+                        track: true,
+                        relative: true
+                    },
+                    yaxis: {
+                        min: 0,
+                        autoscaleMargin: 1,
+                        ticks: [[0,'GR'], [1,'SP'], [2,'DS'], [3,'DC']]
+                    }
+                });
+            })(document.getElementById("masterHeadNode"),true);
+
+
                 //initial update
                 this.update();
 
