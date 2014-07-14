fce9c0baa001acc2beac9e819031e7d719b5cd50
BillMills <mills.wj@gmail.com>
2014-07-14 10:17:32 -0700

    building DAQ bar graphs

--- static/xTags/DAQ/xDAQ.js
+++ static/xTags/DAQ/xDAQ.js
@@ -389,7 +389,7 @@
                     i;
 
                 Flotr.defaultOptions.fontColor = '#EEEEEE';
-                Flotr.defaultOptions.grid.outline = 0;
+                Flotr.defaultOptions.grid.outline = 4;
 
                 for (i = 0; i < 4; i++) {
 
