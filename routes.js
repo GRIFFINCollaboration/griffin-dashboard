db1f8d4af0ead443c296f9e8d6ac0d337e1167ba
BillMills <mills.wj@gmail.com>
2014-07-18 16:51:23 -0700

    fixes #20

--- routes.js
+++ routes.js
@@ -90,8 +90,6 @@ app.get('/MSCbuilder', function(req, res){
 app.post('/postHV', function(req, res){
 
 	spawn('odbedit', ['-c', "set /Equipment/HV-"+req.body.crateIndex+"/Variables/Demand["+req.body.chIndex+"] " + req.body.demandVoltage]);
-	spawn('odbedit', ['-c', "set '/Equipment/HV-"+req.body.crateIndex+"/Settings/Ramp Up Speed["+req.body.chIndex+"]' " + req.body.voltageUp]);
-	spawn('odbedit', ['-c', "set '/Equipment/HV-"+req.body.crateIndex+"/Settings/Ramp Down Speed["+req.body.chIndex+"]' " + req.body.voltageDown]);
 
 	if(req.body.powerSwitch == 'off')
 		spawn('odbedit', ['-c', "set /Equipment/HV-"+req.body.crateIndex+"/Settings/ChState["+req.body.chIndex+"] 0"]);
