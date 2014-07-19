e5b482a8442c75371ef8893ba9652c56a03559c7
BillMills <mills.wj@gmail.com>
2014-07-19 13:20:15 -0700

    assembling shack monitor

--- static/xTags/shack/xShack.js
+++ static/xTags/shack/xShack.js
@@ -875,31 +875,3 @@
     });
 
 })();
-
-//helpers
-function squishFont(string, maxWidth){
-
-    while(string.getTextWidth() > maxWidth){
-        string.setAttr('fontSize', string.getAttr('fontSize') - 1);
-    }
-}
-
-function XHR(URL, callback, mime, noCredentials){
-
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function(){
-
-        if(this.readyState != 4) return;
-        callback(this.responseText);
-    }
-
-    if(!noCredentials)
-        xmlhttp.withCredentials = true;
-
-    if(mime)
-        xmlhttp.overrideMimeType(mime);
-
-    xmlhttp.open('GET', URL);
-    xmlhttp.send();   
-}
