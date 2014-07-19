4488494f29ca73d94b15821c03c26616c1c2412e
BillMills <mills.wj@gmail.com>
2014-07-19 13:28:45 -0700

    assembling shack monitor

--- static/xTags/shack/xShack.js
+++ static/xTags/shack/xShack.js
@@ -52,8 +52,8 @@
 
 			'renderRacks' : function(){
 
-				var width = 0.8*this.innerWidth,
-					height = 0.8*this.innerHeight,
+				var width = 0.8*this.offsetWidth,
+					height = 0.8*this.offsetHeight,
 					grid = Math.min(width/100, height/62),  //20*5 wide, 56+2+2+2 tall
 					cells = {},
 					label = {},
