58276d3692b07330c49a6ef13ba0dda2acaa3db3
BillMills <mills.wj@gmail.com>
2014-07-28 15:47:58 -0700

    enable minification

--- griffin.js
+++ griffin.js
@@ -1,7 +1,7 @@
 MIDAS 		= 'http://grsmid00.triumf.ca:8082'			//wherever mhttpd is serving
 express 	= require("express");		
 app 		= express();				
-//minify 	= require('express-minify');				//minification tool
+minify 		= require('express-minify');				//minification tool
 spawn 		= require('child_process').spawn;			//for spawning system calls
 execFile	= require('child_process').execFile;
 exec 		= require('child_process').exec;
@@ -9,13 +9,13 @@ fs 			= require('fs');							//file system manipulator
 
 // set up the app
 app.set('views', __dirname + '/views');
-/*
+
 app.use(express.compress());
 app.use(minify({
     js_match: /javascript/,
     css_match: /css/
 }));
-*/
+
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.cookieParser());
 app.use(express.bodyParser());										//need this to parse submitted forms
