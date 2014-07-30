6d68f6c6503a6db64f300ceb90821906c5bffc13
BillMills <mills.wj@gmail.com>
2014-07-30 11:59:09 -0700

    reflow hack to fix collapsible menus in chrome

--- static/xTags/detectors/rateSidebar/xRateBar.js
+++ static/xTags/detectors/rateSidebar/xRateBar.js
@@ -226,11 +226,11 @@
 
                     mainSectionH3[i] = document.createElement('h3');
                     mainSectionH3[i].setAttribute('class', 'sectionHead');
-                    mainSectionH3[i].onclick = function(){
+                    mainSectionH3[i].onclick = function(i){
                         toggleSection.bind(mainSectionH3[i], mainSections[i]);
                         //menu partially vanishes in chrome after collapsing a section - force reflow to rectify
                         document.getElementById(this.id).setAttribute('style', 'margin: 1em;')
-                    }.bind(this)
+                    }.bind(this, i)
                     mainSectionH3[i].innerHTML = String.fromCharCode(0x25B6) + ' ' +mainSectionTitles[i];
                     mainSectionDivs[i].appendChild(mainSectionH3[i]);
 
