47d1f17571603eda275cfca1ce34fb052457a7ce
BillMills <mills.wj@gmail.com>
2014-07-31 01:13:18 -0700

    daq plots

--- static/xTags/detectors/daqHistoBar/xDAQhisto.js
+++ static/xTags/detectors/daqHistoBar/xDAQhisto.js
@@ -16,7 +16,7 @@
 
 
                     //draw dygraphs
-                    this.spectrumPlot = new Dygraph(document.getElementById('spectrum'), [1,2,3], {
+                    this.spectrumPlot = new Dygraph(document.getElementById('spectrum'), [[1,2,3],[4,5,6],[7,8,9]], {
                         title: 'test',
                         xlabel: 'ns',
                         ylabel: 'mV',
