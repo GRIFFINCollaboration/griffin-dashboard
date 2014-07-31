6847a4adc1e7b92a9c4c37a476cd11d0bfd386a6
BillMills <mills.wj@gmail.com>
2014-07-31 01:26:19 -0700

    daq plots

--- static/xTags/detectors/daqHistoBar/xDAQhisto.js
+++ static/xTags/detectors/daqHistoBar/xDAQhisto.js
@@ -14,77 +14,6 @@
                         this.appendChild(this[histos[i]]);
                     }
 
-
-                //draw dygraphs
-                this.spectrumPlot = new Dygraph(document.getElementById('spectrum'), [[1,2],[3,4],[5,6]], {
-                    title: 'test',
-                    xlabel: 'ns',
-                    ylabel: 'mV',
-                    width: this.offsetWidth*0.4,
-                    height: this.offsetWidth*0.2,
-                    sigFigs: 2,
-                    strokeWidth: 4,
-                    yAxisLabelWidth: 75,
-                    xAxisHeight: 30,
-                    highlightCircleSize: 6,
-                    showLabelsOnHighlight: false,
-                    stepPlot: true,
-                    includeZero: true,
-                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
-                });
-
-                this.timePlot = new Dygraph(document.getElementById('time'), [[1,2],[3,4],[5,6]], {
-                    title: 'test',
-                    xlabel: 'ns',
-                    ylabel: 'mV',
-                    width: this.offsetWidth*0.4,
-                    height: this.offsetWidth*0.2,
-                    sigFigs: 2,
-                    strokeWidth: 4,
-                    yAxisLabelWidth: 75,
-                    xAxisHeight: 30,
-                    highlightCircleSize: 6,
-                    showLabelsOnHighlight: false,
-                    stepPlot: true,
-                    includeZero: true,
-                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
-                });
-
-                this.pulsePlot = new Dygraph(document.getElementById('pulse'), [[1,2],[3,4],[5,6]], {
-                    title: 'test',
-                    xlabel: 'ns',
-                    ylabel: 'mV',
-                    width: this.offsetWidth*0.4,
-                    height: this.offsetWidth*0.2,
-                    sigFigs: 2,
-                    strokeWidth: 4,
-                    yAxisLabelWidth: 75,
-                    xAxisHeight: 30,
-                    highlightCircleSize: 6,
-                    showLabelsOnHighlight: false,
-                    stepPlot: true,
-                    includeZero: true,
-                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
-                });
-
-                this.wavefromPlot = new Dygraph(document.getElementById('waveform'), [[1,2],[3,4],[5,6]], {
-                    title: 'test',
-                    xlabel: 'ns',
-                    ylabel: 'mV',
-                    width: this.offsetWidth*0.4,
-                    height: this.offsetWidth*0.2,
-                    sigFigs: 2,
-                    strokeWidth: 4,
-                    yAxisLabelWidth: 75,
-                    xAxisHeight: 30,
-                    highlightCircleSize: 6,
-                    showLabelsOnHighlight: false,
-                    stepPlot: true,
-                    includeZero: true,
-                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
-                });
-
-
                 //let repopulate know that the detector would like to be updated every loop:
                 if(!window.refreshTargets)
                     window.refreshTargets = [];
@@ -107,7 +36,7 @@
                 var i,
                     data = [];
 
-                for(i=0; i<4096; i++){
+                for(i=0; i<2048; i++){
                     data.push([i, Math.random()]);
                 }
 
@@ -131,6 +60,65 @@
                     colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
                 });
 
+                if(this.timePlot)
+                    this.timePlot.destroy()
+
+                this.timePlot = new Dygraph(document.getElementById('time'), data, {
+                    title: 'test',
+                    xlabel: 'ns',
+                    ylabel: 'mV',
+                    width: this.offsetWidth*0.4,
+                    height: this.offsetWidth*0.2,
+                    sigFigs: 2,
+                    strokeWidth: 4,
+                    yAxisLabelWidth: 75,
+                    xAxisHeight: 30,
+                    highlightCircleSize: 6,
+                    showLabelsOnHighlight: false,
+                    stepPlot: true,
+                    includeZero: true,
+                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                });
+
+                if(this.pulsePlot)
+                    this.pulsePlot.destroy()
+
+                this.spectrumPlot = new Dygraph(document.getElementById('pulse'), data, {
+                    title: 'test',
+                    xlabel: 'ns',
+                    ylabel: 'mV',
+                    width: this.offsetWidth*0.4,
+                    height: this.offsetWidth*0.2,
+                    sigFigs: 2,
+                    strokeWidth: 4,
+                    yAxisLabelWidth: 75,
+                    xAxisHeight: 30,
+                    highlightCircleSize: 6,
+                    showLabelsOnHighlight: false,
+                    stepPlot: true,
+                    includeZero: true,
+                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                });
+
+                if(this.waveformPlot)
+                    this.waveformPlot.destroy()
+
+                this.spectrumPlot = new Dygraph(document.getElementById('waveform'), data, {
+                    title: 'test',
+                    xlabel: 'ns',
+                    ylabel: 'mV',
+                    width: this.offsetWidth*0.4,
+                    height: this.offsetWidth*0.2,
+                    sigFigs: 2,
+                    strokeWidth: 4,
+                    yAxisLabelWidth: 75,
+                    xAxisHeight: 30,
+                    highlightCircleSize: 6,
+                    showLabelsOnHighlight: false,
+                    stepPlot: true,
+                    includeZero: true,
+                    colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                });
 
             }
 
