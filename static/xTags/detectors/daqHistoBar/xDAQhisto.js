9d9df21eb75ea412b50a1a0731be8450413d658c
BillMills <mills.wj@gmail.com>
2014-07-31 00:55:55 -0700

    daq plots

--- static/xTags/detectors/daqHistoBar/xDAQhisto.js
+++ static/xTags/detectors/daqHistoBar/xDAQhisto.js
@@ -25,6 +25,52 @@
                         yAxisLabelWidth: 75,
                         xAxisHeight: 30,
                         highlightCircleSize: 6,
+                        showLabelsOnHighlight: false,
+                        stepPlot: true,
+                        includeZero: true,
+                        colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                    });
+
+                    this.timePlot = new Dygraph(document.getElementById('time'), 'ns,mV,\n', {
+                        title: 'test',
+                        xlabel: 'ns',
+                        ylabel: 'mV',
+                        sigFigs: 2,
+                        strokeWidth: 4,
+                        yAxisLabelWidth: 75,
+                        xAxisHeight: 30,
+                        highlightCircleSize: 6,
+                        showLabelsOnHighlight: false,
+                        stepPlot: true,
+                        includeZero: true,
+                        colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                    });
+
+                    this.pulsePlot = new Dygraph(document.getElementById('pulse'), 'ns,mV,\n', {
+                        title: 'test',
+                        xlabel: 'ns',
+                        ylabel: 'mV',
+                        sigFigs: 2,
+                        strokeWidth: 4,
+                        yAxisLabelWidth: 75,
+                        xAxisHeight: 30,
+                        highlightCircleSize: 6,
+                        titleHeight: 50,
+                        showLabelsOnHighlight: false,
+                        stepPlot: true,
+                        includeZero: true,
+                        colors: ['#F1C40F', '#2ECC71', '#E74C3C', '#ECF0F1', '#1ABC9C', '#E67E22', '#9B59B6']
+                    });
+
+                    this.wavefromPlot = new Dygraph(document.getElementById('waveform'), 'ns,mV,\n', {
+                        title: 'test',
+                        xlabel: 'ns',
+                        ylabel: 'mV',
+                        sigFigs: 2,
+                        strokeWidth: 4,
+                        yAxisLabelWidth: 75,
+                        xAxisHeight: 30,
+                        highlightCircleSize: 6,
                         titleHeight: 50,
                         showLabelsOnHighlight: false,
                         stepPlot: true,
