ab6a89487e90e3c435cd976d57a0ed94bdf16933
BillMills <mills.wj@gmail.com>
2014-07-31 00:19:47 -0700

    infrastructure for per channel daq plots

--- static/xTags/detectors/detectorHelpers.js
+++ static/xTags/detectors/detectorHelpers.js
@@ -107,9 +107,10 @@ function initializeDetector(name, headline){
     xString += '<x-card id="DAQhistoCard"><div id="DAQspectrum" class="DAQhisto"></div><div id="DAQpulse" class="DAQhisto"></div><div id="DAQtime" class="DAQhisto"></div><div id="DAQwaveform" class="DAQhisto"></div></x-card>'
     xString += '</x-deck>'
     deckWrap.innerHTML = xString;
+    this.viewNames.push('Plots')
 
     //plot buffers
-    for(i=0; i<this.viewNames.length; i++){  //exclude last card, special for daq histos
+    for(i=0; i<this.viewNames.length - 1; i++){  //exclude last card, special for daq
         //divs to hold kinetic contexts
         drawTarget = document.createElement('div');
         drawTarget.setAttribute('id', this.id+this.viewNames[i]+'Draw');
@@ -244,7 +245,7 @@ function initializeDetector(name, headline){
     this.mainLayer = [];
     this.scaleLayer = [];
 
-    for(i=0; i<this.viewNames.length; i++){
+    for(i=0; i<this.viewNames.length - 1; i++){
 
         //point kinetic at the div and set up the staging and layers:
         this.stage[i] = new Kinetic.Stage({
