e1f60bc1d16f0f7ca6a6e25e58300926a950fe17
Adam Garnsworthy <garns@wlan2-162.triumf.ca>
2019-04-03 10:55:16 -0700

    Added URL registering of backend hostname

--- scripts/dataStore.js
+++ scripts/dataStore.js
@@ -680,13 +680,15 @@ dataStore = {
 }
 
 // Grab the hostname from the URL
+function setHostname(backend){
 if(backend=="griffin")
 {
     dataStore.host = 'grsmid00.triumf.ca:8081';
 }else if(backend=="tigress"){
     dataStore.host = 'grsmid02.triumf.ca:8081';
-}else{
+}else
     dataStore.host = 'missing-hostname';
+
 }
 
 dataStore.runSummaryQuery = 'http://'+dataStore.host+'/?cmd=jcopy&odb0=Experiment/&odb1=Runinfo/&odb2=Equipment/Trigger/Statistics/&odb3=Logger/&encoding=json-p-nokeys&callback=runSummaryCB';
